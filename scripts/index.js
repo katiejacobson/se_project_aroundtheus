d5f8484411b4930182563e47bcf507d5cfe3a08a
Katie Jacobson <katieabole@gmail.com>
2024-04-04 15:09:54 -0400

    card rendering

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,37 @@ profileEditButton.addEventListener("click", function () {
 modalCloseButton.addEventListener("click", function () {
   profileEditModal.classList.remove("modal_opened");
 });
+
+const profileFormElement = document.querySelector(".modal__form");
+
+const nameInput = document.querySelector(".modal__title");
+const jobInput = document.querySelector(".modal__description");
+
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  profileEditModal.classList.toggle("modal_opened");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function getCardElement(data) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardTitle = data.name;
+  let cardImage = data.link;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__image").setAttribute("alt", cardTitle);
+  return cardElement;
+}
+
+let galleryCards = document.querySelector(".gallery__cards");
+
+for (let i = 0; i < initialCards.length; i++) {
+  galleryCards.append(getCardElement(initialCards[i]));
+}
